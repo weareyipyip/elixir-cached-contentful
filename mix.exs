b6f2ec17f2e7aa954febfbbf693e56bcd0616c7f
Jonathan van Putten <jonathan@MBPvanJonathan.fritz.box>
2017-06-29 13:45:19 +0200

    Updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CachedContentful.Mixfile do
 
   def project do
     [app: :cached_contentful,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
