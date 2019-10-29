8d9a4d96bfa7a6f015180360d62c705f745cdf5e
Jonathan van Putten <j.vanputten@yipyip.nl>
2019-10-29 12:36:47 +0100

    Added environments

--- config/config.exs
+++ config/config.exs
@@ -5,5 +5,6 @@ use Mix.Config
  config :cached_contentful,
     space_id: "spaceid",
     access_token: "accesstoken",
+    environment: "master",
     auto_update: false,
     update_interval: 24 * 60 * 60 * 1000 # Refresh every 24 hours (milliseconds)
--- lib/request_handler.ex
+++ lib/request_handler.ex
@@ -2,27 +2,32 @@ defmodule CachedContentful.RequestHandler do
 
 	defp get_env_space_id(), do: Application.get_env(:cached_contentful, :space_id)
 	defp get_env_access_token(), do: Application.get_env(:cached_contentful, :access_token)
+	defp get_env_contentful_env(), do: Application.get_env(:cached_contentful, :environment)
 
 	@base_url "https://cdn.contentful.com"
 
 	def get_all_entries() do
 		space_id = get_env_space_id()
 		access_token = get_env_access_token()
-		url = "#{@base_url}/spaces/#{space_id}/entries?access_token=#{access_token}&locale=*&limit=900"
+		environment = get_env_contentful_env()
+		url = "#{@base_url}/spaces/#{space_id}/environments/#{environment}/entries?access_token=#{access_token}&locale=*&limit=900"
+		IO.inspect url
 		process_request(url)
 	end
 
 	def get_all_assets() do
 		space_id = get_env_space_id()
 		access_token = get_env_access_token()
-		url = "#{@base_url}/spaces/#{space_id}/assets?access_token=#{access_token}&limit=900"
+		environment = get_env_contentful_env()
+		url = "#{@base_url}/spaces/#{space_id}/environments/#{environment}/assets?access_token=#{access_token}&limit=900"
 		process_request(url)
 	end
 
 	def custom_query(query) do
 		space_id = get_env_space_id()
 		access_token = get_env_access_token()
-		url = "#{@base_url}/spaces/#{space_id}/entries?access_token=#{access_token}&locale=*#{query}&limit=900"
+		environment = get_env_contentful_env()
+		url = "#{@base_url}/spaces/#{space_id}/environments/#{environment}/entries?access_token=#{access_token}&locale=*#{query}&limit=900"
 		process_request(url)
 	end
 
@@ -32,6 +37,9 @@ defmodule CachedContentful.RequestHandler do
 				items = body |> Poison.decode!
 				items["items"]
 			%HTTPoison.Response{status_code: 401, body: body} ->
+				errors = body |> Poison.decode!
+				{:error, errors}
+			%HTTPoison.Response{status_code: 404, body: body} ->
 				errors = body |> Poison.decode!
 				{:error, errors}
 		end
